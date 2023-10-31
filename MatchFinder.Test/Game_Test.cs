147a4c4c8c1ce9c33af34c529e7b50a11f0b0988
xuanphuoc92 <xuanphuoc92@gmail.com>
2023-10-31 22:03:56 +0800

    Implement New and Add

--- MatchFinder.Test/Game_Test.cs
+++ MatchFinder.Test/Game_Test.cs
@@ -1,11 +1,17 @@
+using FluentAssertions;
+
 namespace MatchFinder.Test
 {
     [TestClass]
     public class Game_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void _01_NewAdd()
         {
+            Game game = Game.New();
+            game.Cells.Should().HaveCount(0);
+            game.Add(0);
+            game.Cells.Should().HaveCount(1);
         }
     }
 }
--- MatchFinder/Game.cs
+++ MatchFinder/Game.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MatchFinder
 {
     public class Game
     {
+        public List<Cell> Cells { get; private set; }
+        private Game() 
+        {
+            Cells = new List<Cell>();
+        }
 
+        public static Game New() { return new Game(); }
+        public Game Add(int cellContent)
+        {
+            Cells.Add(new Cell() { Content = cellContent });
+            return this;
+        }
+    }
+
+    public class Cell
+    {
+        public int Content { get; set; }
     }
 }
