0d111b1f9ae51173678abee9af232cc98d8a21f1
xuanphuoc92 <xuanphuoc92@gmail.com>
2023-10-31 23:43:51 +0800

    Inject and implement Front-end

--- MatchFinder.Mud/Program.cs
+++ MatchFinder.Mud/Program.cs
@@ -1,3 +1,4 @@
+using MatchFinder;
 using MatchFinder.Mud;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => Game.New().Reset().AddRandoms(30));
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
